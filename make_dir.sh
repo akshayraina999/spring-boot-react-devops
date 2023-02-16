75bf116ad3ac1390dd3b5b0294ba00c9316beb11
Akshay Raina <akshayraina999@gmail.com>
2023-02-16 12:48:37 +0530

    [FIX] directory path name

--- make_dir.sh
+++ make_dir.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ ! -e $1 ]]; then
-    mkdir $1
-elif [[ ! -d $1 ]]; then
-    echo "$1 already exists but is not a directory" 1>&2
+if [[ ! -e /home/ubuntu/$1 ]]; then
+    mkdir /home/ubuntu/$1
+elif [[ ! -d /home/ubuntu/$1 ]]; then
+    echo "/home/ubuntu/$1 already exists but is not a directory" 1>&2
 fi
 
